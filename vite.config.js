641c5d069784efad8fa7724eae08d2791df5e61c
kireiigoat <kireiigoat@example.com>
2025-08-03 12:59:23 +0200

    fix: set root to current directory for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.', // <-- wichtig!
   plugins: [react()],
   build: {
     outDir: 'dist'
   }
 })
+
